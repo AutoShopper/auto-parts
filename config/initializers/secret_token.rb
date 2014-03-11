aebf8317a2836a3bc2d88c5f820ddad0b34d908e
dany <dany@dany-laptop.(none)>
2014-03-10 22:56:29 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AutoParts::Application.config.secret_key_base = '315f83fc02cf71f927f0796761b1765aea5dca0af2f422d27e20571a9d563d604b91fbd99e28693a4b4ac862141cffb2e0f17690b619069102538d8dcd7c9a8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AutoParts::Application.config.session_store :cookie_store, key: '_auto-parts_session'
